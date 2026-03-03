1d6378f5acc4e4287922defc250d33224b0818ad
Adam DePetris <adamdepetris@pm.me>
2026-03-03 12:43:40 -0500

    rename variables for clarity

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum command
+enum builtin
 {
   unknown,
   echo,
@@ -10,48 +10,49 @@ enum command
   type,
 };
 
-enum command parse_command(char *command)
+enum builtin parse_buffer(char *buffer)
 {
-  char *space = strchr(command, ' ');
-  int command_len = space ? space-command : (int)strlen(command);
+  const char *space = strchr(buffer, ' ');
+  const int command_len = space ? space - buffer : (int)strlen(buffer);
   if (command_len == 0)
     return unknown;
 
-  if (strncmp(command, "echo", command_len) == 0)
+  if (strncmp(buffer, "echo", command_len) == 0)
     return echo;
-  if (strncmp(command, "exit", command_len) == 0)
+  if (strncmp(buffer, "exit", command_len) == 0)
     return exit_command;
-  if (strncmp(command, "type", command_len) == 0)
+  if (strncmp(buffer, "type", command_len) == 0)
     return type;
   return unknown;
 }
 
 int main(int argc, char *argv[]) {
-  char command[1024];
+  char buffer[1024];
   while (1)
   {
     setbuf(stdout, nullptr);
     printf("$ ");
-    fgets(command, sizeof(command), stdin);
+    fgets(buffer, sizeof(buffer), stdin);
     //Trim trailing newline. I don't exactly like this solution, probably gonna have to replace later if we need multi line support.
-    command[strcspn(command, "\n")] = '\0';
+    buffer[strcspn(buffer, "\n")] = '\0';
 
-    switch (parse_command(command))
+
+    switch (parse_buffer(buffer))
     {
     case echo:
-      printf("%s\n", command+5);
+      printf("%s\n", buffer+5);
       break;
     case exit_command:
       exit(0);
       break;
     case type:
-      if (parse_command(command+5) != unknown)
-        printf("%s is a shell builtin\n", command+5);
+      if (parse_buffer(buffer+5) != unknown)
+        printf("%s is a shell builtin\n", buffer+5);
       else
-        printf("%s: not found\n", command+5);
+        printf("%s: not found\n", buffer+5);
       break;
     case unknown:
-      printf("%s: command not found\n", command);
+      printf("%s: command not found\n", buffer);
       break;
     }
   }
